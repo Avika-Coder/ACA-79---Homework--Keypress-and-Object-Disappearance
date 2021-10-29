5b4c2d931e000c3557f103bc26a667b7b1b79c25
Avika-Coder <76557800+Avika-Coder@users.noreply.github.com>
2021-10-29 16:22:04 +0530

    Add files via upload

---
+++ ACA-79-Homework/Apple.js
@@ -0,0 +1,23 @@
+class Apple extends BaseClass{
+    constructor(x, y){
+        super(x, y, 50, 50);
+        this.image = loadImage("./sprites/apple.png");
+        this.visiblity = 255;
+    }
+
+    display() {
+        if(this.body.speed < 2) {
+            super.display();
+        }
+        else {
+            Matter.World.remove(world, this.body);
+            this.visiblity = this.visiblity - 5;
+            push();
+            tint(255,this.visiblity);
+            image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+            pop();
+        }
+    }
+
+
+}
